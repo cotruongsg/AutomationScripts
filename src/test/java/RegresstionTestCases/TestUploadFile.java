3b87733db3f0446561f8d3f02862942fe3135f02
cotruongsg <cotruongsg@yahoo.com>
2020-10-21 14:56:53 -0700

    upload

---
+++ src/test/java/RegresstionTestCases/TestUploadFile.java
@@ -0,0 +1,35 @@
+package RegresstionTestCases;
+
+import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import Utils.BrowserFactory;
+import Utils.CommonActions;
+
+
+public class TestUploadFile
+{
+	  CommonActions cact = new CommonActions();
+	  @Test
+	  public void TestUpload() throws IOException
+	  {
+		  WebDriver driver = BrowserFactory.getBrowser("firefox");
+		  driver.get("https://fineuploader.com/demos.html");
+		  driver.manage().window().maximize();
+		  cact.ImplicitWait(driver, 10);
+		  WebElement select_btn = driver.findElement(By.xpath("//div[starts-with(@class,'qq-upload-button-selector qq-upload-button')]/div[contains(text(),'Select files')]"));
+		  JavascriptExecutor jse = (JavascriptExecutor)driver;		  
+		  jse.executeScript("window.scrollBy(0,350)");
+		  //Actions act = new Actions(driver);
+		  //act.moveToElement(select_btn).click().perform();		 
+		  driver.switchTo().activeElement().sendKeys("D:\\TestUpload.txt");
+		 
+		  
+		  
+	  }
+}
+
