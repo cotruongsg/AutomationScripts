0ebff03ab6f60fa43d4e84d7b3b195078952223e
Bonbon525 <Bonbon525@10.0.0.252>
2020-12-01 20:47:54 -0800

    Remove Unused import

--- src/test/java/Utils/BrowserFactory.java
+++ src/test/java/Utils/BrowserFactory.java
@@ -1,18 +1,12 @@
 package Utils;
 
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 //Class Name
